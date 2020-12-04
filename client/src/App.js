b6865ecbae5ae138a5f7d3d3752faef21fcd6c93
Steven Loewen <stevenloewen@hotmail.com>
2020-12-04 11:11:58 -0500

    Starting the Sign In Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
-import './styles/main.css';
+// import './styles/main.css';
 import './App.scss';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import UserPage from './UserPage';
 import UserItemPage from './UserItemPage';
 import HomePage from './HomePage';
+import SignInPage from './SignInPage';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path = "/" exact component = {HomePage} />
+          {/* <SignInPage/> */}
+          <Route path = "/" exact component = {SignInPage} />
+          {/* <Route path = "/" exact component = {HomePage} /> */}
           <Route path = "/user" exact component = {UserPage} />
           <Route path = "/user/item" exact component = {UserItemPage} />          
         </Switch>
---
+++ client/src/SignInPage.js
@@ -0,0 +1,18 @@
+import './SignInPage.scss';
+
+import React from 'react';
+
+function SignInPage(props) {
+    return (
+        <div className="sign-in-page">
+            <div className="sign-in-card">
+                <img src="./assets/icons/walmart-main-logo-1.jpg" alt="walmart logo" className="logo"/>
+                
+
+
+            </div>            
+        </div>
+    )
+};
+
+export default SignInPage;
