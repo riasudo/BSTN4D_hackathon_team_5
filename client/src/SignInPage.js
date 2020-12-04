66512affa928a7b9a21e172ea0276b867ebd7a17
Steven Loewen <stevenloewen@hotmail.com>
2020-12-04 13:22:12 -0500

    Profiles page done, but no linking

--- client/src/SignInPage.js
+++ client/src/SignInPage.js
@@ -7,7 +7,8 @@ function SignInPage(props) {
     return (
         <div className="sign-in-page">
             <div className="sign-in-card">
-                <img src="./assets/icons/walmart-main-logo-1.jpg" alt="walmart logo" className="logo"/>
+                {/* <img src="./assets/icons/walmart-main-logo-1.jpg" alt="walmart logo" className="logo"/> */}
+                <div className="logo"></div>
                 <h1 className="sign-in">Sign In</h1>
                 <p className="email-phone">Email Address or Phone Number</p>
                 <div type="text" className="email-phone-input">
@@ -15,7 +16,8 @@ function SignInPage(props) {
                 </div>
                 <p className="password">Password</p>
                 <div className="password-input">
-                    <img src="./assets/icons/eye.png" alt="show password" className="email-phone-input_eye"/>
+                    {/* <img src="./assets/icons/eye.png" alt="show password" className="email-phone-input_eye"/> */}
+                    <div className="email-phone-input__eye"></div>
                     <p className="email-phone-input__text">Show</p>
                 </div>
                 <p className="forgot-password">Forgot your password?</p>
