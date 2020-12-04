4dfa129c813672a9015bdefe28f74926ca5a7430
Steven Loewen <stevenloewen@hotmail.com>
2020-12-03 22:04:00 -0500

    Initial routing and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import './styles/main.css';
+import './App.scss';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import UserPage from './UserPage';
+import UserItemPage from './UserItemPage';
+import HomePage from './HomePage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path = "/" exact component = {HomePage} />
+          <Route path = "/user" exact component = {UserPage} />
+          <Route path = "/user/item" exact component = {UserItemPage} />          
+        </Switch>
+      </BrowserRouter>
+      
     </div>
   );
 }
 
 export default App;
+
---
+++ client/src/UserPage.js
@@ -0,0 +1,13 @@
+import './UserPage.scss';
+import { Link, NavLink } from 'react-router-dom';
+import React, {Component} from 'react';
+
+function UserPage(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default UserPage;
