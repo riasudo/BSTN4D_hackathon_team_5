7e585415998b85c74e0afe969ae1ccd01fe9290f
Ivan Yang <0201ivan@gmail.com>
2020-12-04 10:53:16 -0800

    created seed data

---
+++ server/seed_data/products.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        name: "The Classic Bomber Jacket",
+        category: "tops",
+        brand: "BSTN Apparel",
+        description: "Water resistant and warm, this jacket is a classic fashionable choice.",
+    },
+    {
+        name: "The Classic Trousers",
+        category: "bottoms",
+        brand: "BSTN Apparel",
+        description: "Comfy and timeless, this is a great pair of pants for any occasion."
+    },
+    {
+        name: "The Classic Loafers",
+        category: "shoes",
+        brand: "BSTN Apparel",
+        description: "Clean, and modern, these breathable shoes will prepare you for any journey."
+    }
+]
---
+++ server/seed_data/users.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        name: "Peter",
+        age: "35",
+        avatar: ""
+    },
+    {
+        name: "Jenny",
+        age: "30",
+        avatar: ""
+    }
+]
